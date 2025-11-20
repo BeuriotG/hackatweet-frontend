47a345d17dd3e9b35394a9d5cbcc5c40cf4b262e
Jb <deslandes.jb@gmail.com>
2025-11-20 14:34:52 +0100

    hp + auth in & out OK

--- components/AuthIn.js
+++ components/AuthIn.js
@@ -5,24 +5,25 @@ import {Label} from '@/components/ui/label';
 import {useState} from 'react';
 
 export function Auth() {
+  const [signInUsername, setSignInUsername] = useState('');
+  const [signInPassword, setSignInPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const [signUpName, setSignUpName] = useState('');
-  const [signUpUsername, setSignUpUsername] = useState('');
-  const [signUpPassword, setSignUpPassword] = useState('');
 
-  const handleRegister = () => {
-    fetch('http://localhost:3000/users/signup', {
+  const handleConnection = () => {
+    fetch('http://localhost:3000/users/signin', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({firstname: signUpName, username: signUpUsername, password: signUpPassword}),
+      body: JSON.stringify({username: signInUsername, password: signInPassword}),
     })
       .then((response) => response.json())
       .then((data) => {
         if (data.result) {
-          // dispatch(login({username: signUpUsername, token: data.token}));
-          setSignUpName('');
-          setSignUpUsername('');
-          setSignUpPassword('');
+          // dispatch(login({username: signInUsername, token: data.token}));
+
+          setSignInUsername('');
+          setSignInPassword('');
+
+          // setIsModalVisible(false);
           console.log(data);
         } else {
           setErrorMessage(data.error);
@@ -37,33 +38,29 @@ export function Auth() {
     <Dialog>
       <form>
         <DialogTrigger asChild>
-          <Button variant="outline">Créer compte</Button>
+          <Button variant="default">Me connecter</Button>
         </DialogTrigger>
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
-            <DialogTitle>Créer mon compte</DialogTitle>
+            <DialogTitle>Connexion</DialogTitle>
             <DialogDescription className="text-red-600 italic">{errorMessage}</DialogDescription>
           </DialogHeader>
           <div className="grid gap-4">
             <div className="grid gap-3">
-              <Label htmlFor="setSignUpName">Prénom</Label>
-              <Input id="setSignUpName" name="name" onChange={(e) => setSignUpName(e.target.value)} />
+              <Label htmlFor="setSignInUsername">Username</Label>
+              <Input id="setSignInUsername" name="username" onChange={(e) => setSignInUsername(e.target.value)} />
             </div>
             <div className="grid gap-3">
-              <Label htmlFor="setSignUpUsername">Username</Label>
-              <Input id="setSignUpUsername" name="username" onChange={(e) => setSignUpUsername(e.target.value)} />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="setSignUpPassword">Password</Label>
-              <Input id="setSignUpPassword" type="password" name="password" onChange={(e) => setSignUpPassword(e.target.value)} />
+              <Label htmlFor="setSignInPassword">Password</Label>
+              <Input id="setSignInPassword" type="password" name="password" onChange={(e) => setSignInPassword(e.target.value)} />
             </div>
           </div>
           <DialogFooter>
             <DialogClose asChild>
               <Button variant="outline">Retour</Button>
             </DialogClose>
-            <Button type="submit" onClick={() => handleRegister()}>
-              Créer mon compte
+            <Button type="submit" onClick={() => handleConnection()}>
+              Connexion
             </Button>
           </DialogFooter>
         </DialogContent>
--- components/Home.js
+++ components/Home.js
@@ -1,3 +1,4 @@
+import AuthIn from './AuthIn';
 import AuthUp from './AuthUp';
 function Home() {
   return (
@@ -5,9 +6,14 @@ function Home() {
       <main className="">
         <section className="bg-pink-50 min-h-screen flex">
           <div className="bg-[url('https://cdn.pixabay.com/photo/2025/06/04/22/43/facade-9641925_1280.jpg')] min-h-screen w-1/2 bg-cover bg-center"></div>
-          <div className="min-h-screen w-1/2">
-            <h1 className="font-mono text-8xl">See what's happening</h1>
-            <AuthUp />
+          <div className="min-h-screen w-1/2 flex items-center justify-center">
+            <div className="px-6 flex flex-col gap-12">
+              <h1 className="font-mono text-8xl">Welcome back&nbsp;!</h1>
+              <div className="flex gap-4">
+                <AuthUp />
+                <AuthIn />
+              </div>
+            </div>
           </div>
         </section>
       </main>
