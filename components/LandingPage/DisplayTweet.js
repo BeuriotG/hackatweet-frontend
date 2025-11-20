9b84f84df2181330f8880a4941ab8b1c49a743ed
Jb <deslandes.jb@gmail.com>
2025-11-20 17:29:00 +0100

    show tweets on the LP

--- components/LandingPage/DisplayTweet.js
+++ components/LandingPage/DisplayTweet.js
@@ -1,29 +1,23 @@
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card.tsx";
+import {Card, CardContent, CardFooter, CardHeader, CardTitle} from '@/components/ui/card.tsx';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import {faHeart, faTrashCan} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 export function DisplayTweet(props) {
-  return (
-    <Card className="w-full h-[200px] bg-inherit text-white rounded-none">
-      <CardHeader className="flex flex-row justify-start">
-        <CardTitle>
-          {props.firstname} @{props.username} - {props.hours}hours ago
-        </CardTitle>
-      </CardHeader>
-      <CardContent>{props.tweet}</CardContent>
-      <CardFooter>
-        <FontAwesomeIcon icon={faHeart} /> {props.nbOfLikes}
-        <FontAwesomeIcon icon={faTrashCan} />
-      </CardFooter>
-    </Card>
-  );
+	return (
+		<Card className="h-[200px] w-full rounded-none bg-inherit text-white">
+			<CardHeader className="flex flex-row justify-start">
+				<CardTitle>
+					{props.firstname} @{props.username} - {props.hours}hours ago
+				</CardTitle>
+			</CardHeader>
+			<CardContent>{props.tweet}</CardContent>
+			<CardFooter>
+				<FontAwesomeIcon icon={faHeart} /> {props.nbOfLikes}
+				<FontAwesomeIcon icon={faTrashCan} />
+			</CardFooter>
+		</Card>
+	);
 }
 
 export default DisplayTweet;
