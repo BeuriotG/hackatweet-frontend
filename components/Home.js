d386e50323714d45be778cdba256ada1ff6618c8
Jb <deslandes.jb@gmail.com>
2025-11-20 11:15:23 +0100

    starting hp

--- components/Home.js
+++ components/Home.js
@@ -2,10 +2,13 @@ import styles from '../styles/Home.module.css';
 function Home() {
   return (
     <div>
-      <main className="bg-red-500">
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main className="">
+        <section className="bg-lime-50 min-h-screen">
+          <div className="bg-[url('https://cdn.pixabay.com/photo/2025/06/04/22/43/facade-9641925_1280.jpg')] min-h-screen w-1/2"></div>
+          <div className="min-h-screen w-1/2">
+            <h1>See what's happening</h1>
+          </div>
+        </section>
       </main>
     </div>
   );
