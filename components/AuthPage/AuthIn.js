3618700e6bb71234de66177f2c05e14882cc6ac4
Jb <deslandes.jb@gmail.com>
2025-11-20 15:38:47 +0100

    redux setup ok

---
+++ components/AuthPage/AuthIn.js
@@ -0,0 +1,73 @@
+import {Button} from '@/components/ui/button';
+import {Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger} from '@/components/ui/dialog';
+import {Input} from '@/components/ui/input';
+import {Label} from '@/components/ui/label';
+import {useRouter} from 'next/router';
+import {useState} from 'react';
+
+export function AuthInComponent() {
+	const router = useRouter();
+	const [signInUsername, setSignInUsername] = useState('');
+	const [signInPassword, setSignInPassword] = useState('');
+	const [errorMessage, setErrorMessage] = useState('');
+
+	const handleConnection = () => {
+		fetch('http://localhost:3000/users/signin', {
+			method: 'POST',
+			headers: {'Content-Type': 'application/json'},
+			body: JSON.stringify({username: signInUsername, password: signInPassword}),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				if (data.result) {
+					dispatch(login({username: signInUsername, token: data.token}));
+
+					setSignInUsername('');
+					setSignInPassword('');
+
+					// redirect to the LP
+					router.push('/landingPage');
+				} else {
+					setErrorMessage(data.error);
+				}
+			});
+	};
+
+	// -------------------------------------------------------------------------------------
+	// -------------------------------------------------------------------------------------
+	// -------------------------------------------------------------------------------------
+	return (
+		<Dialog>
+			<form>
+				<DialogTrigger asChild>
+					<Button variant="default">Me connecter</Button>
+				</DialogTrigger>
+				<DialogContent className="sm:max-w-[425px]">
+					<DialogHeader>
+						<DialogTitle>Connexion</DialogTitle>
+						<DialogDescription className="italic text-red-600">{errorMessage}</DialogDescription>
+					</DialogHeader>
+					<div className="grid gap-4">
+						<div className="grid gap-3">
+							<Label htmlFor="setSignInUsername">Username</Label>
+							<Input id="setSignInUsername" name="username" onChange={(e) => setSignInUsername(e.target.value)} />
+						</div>
+						<div className="grid gap-3">
+							<Label htmlFor="setSignInPassword">Password</Label>
+							<Input id="setSignInPassword" type="password" name="password" onChange={(e) => setSignInPassword(e.target.value)} />
+						</div>
+					</div>
+					<DialogFooter>
+						<DialogClose asChild>
+							<Button variant="outline">Retour</Button>
+						</DialogClose>
+						<Button type="submit" onClick={() => handleConnection()}>
+							Connexion
+						</Button>
+					</DialogFooter>
+				</DialogContent>
+			</form>
+		</Dialog>
+	);
+}
+export default AuthInComponent;
