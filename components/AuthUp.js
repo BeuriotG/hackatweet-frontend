9fa42afd145d2ca0cec34fd900b1d5d100f330de
Jb <deslandes.jb@gmail.com>
2025-11-20 11:49:51 +0100

    create account ok

--- components/AuthUp.js
+++ components/AuthUp.js
@@ -2,34 +2,66 @@ import {Button} from '@/components/ui/button';
 import {Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger} from '@/components/ui/dialog';
 import {Input} from '@/components/ui/input';
 import {Label} from '@/components/ui/label';
+import {useState} from 'react';
 
 export function Auth() {
+  const [signUpName, setSignUpName] = useState('');
+  const [signUpUsername, setSignUpUsername] = useState('');
+  const [signUpPassword, setSignUpPassword] = useState('');
+
+  const handleRegister = () => {
+    fetch('http://localhost:3000/users/signup', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({firstname: signUpName, username: signUpUsername, password: signUpPassword}),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          // dispatch(login({username: signUpUsername, token: data.token}));
+          setSignUpName('');
+          setSignUpUsername('');
+          setSignUpPassword('');
+          console.log(data);
+        }
+      });
+  };
+
+  // -------------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------
   return (
     <Dialog>
       <form>
         <DialogTrigger asChild>
-          <Button variant="outline">Open Dialog</Button>
+          <Button variant="outline">Créer compte</Button>
         </DialogTrigger>
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>Make changes to your profile here. Click save when you&apos;re done.</DialogDescription>
+            <DialogTitle>Créer mon compte</DialogTitle>
+            {/* <DialogDescription>Make changes to your profile here. Click save when you&apos;re done.</DialogDescription> */}
           </DialogHeader>
           <div className="grid gap-4">
             <div className="grid gap-3">
-              <Label htmlFor="name-1">Name</Label>
-              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+              <Label htmlFor="setSignUpName">Prénom</Label>
+              <Input id="setSignUpName" name="name" onChange={(e) => setSignUpName(e.target.value)} />
             </div>
             <div className="grid gap-3">
-              <Label htmlFor="username-1">Username</Label>
-              <Input id="username-1" name="username" defaultValue="@peduarte" />
+              <Label htmlFor="setSignUpUsername">Username</Label>
+              <Input id="setSignUpUsername" name="username" onChange={(e) => setSignUpUsername(e.target.value)} />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="setSignUpPassword">Password</Label>
+              <Input id="setSignUpPassword" type="password" name="password" onChange={(e) => setSignUpPassword(e.target.value)} />
             </div>
           </div>
           <DialogFooter>
             <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
+              <Button variant="outline">Retour</Button>
             </DialogClose>
-            <Button type="submit">Save changes</Button>
+            <Button type="submit" onClick={() => handleRegister()}>
+              Créer mon compte
+            </Button>
           </DialogFooter>
         </DialogContent>
       </form>
--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,13 @@
-import styles from '../styles/Home.module.css';
+import AuthUp from './AuthUp';
 function Home() {
   return (
     <div>
       <main className="">
-        <section className="bg-lime-50 min-h-screen">
-          <div className="bg-[url('https://cdn.pixabay.com/photo/2025/06/04/22/43/facade-9641925_1280.jpg')] min-h-screen w-1/2"></div>
+        <section className="bg-pink-50 min-h-screen flex">
+          <div className="bg-[url('https://cdn.pixabay.com/photo/2025/06/04/22/43/facade-9641925_1280.jpg')] min-h-screen w-1/2 bg-cover bg-center"></div>
           <div className="min-h-screen w-1/2">
-            <h1>See what's happening</h1>
+            <h1 className="font-mono text-8xl">See what's happening</h1>
+            <AuthUp />
           </div>
         </section>
       </main>
