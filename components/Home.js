3aaf954491572bfff06fa0449f3498ee32f13f08
Jb <deslandes.jb@gmail.com>
2025-11-20 10:33:47 +0100

    setup tailwind OK

--- components/Home.js
+++ components/Home.js
@@ -3,8 +3,8 @@ import styles from '../styles/Home.module.css';
 function Home() {
   return (
     <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main className="bg-red-500">
+        <h1>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
       </main>
