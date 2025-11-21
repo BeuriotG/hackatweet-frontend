bca050ef638b897055bb2ff40bdea146a2758702
Jb <deslandes.jb@gmail.com>
2025-11-21 15:50:11 +0100

    show trends

--- components/LandingPage/TrendsList.js
+++ components/LandingPage/TrendsList.js
@@ -2,30 +2,21 @@ import {Card, CardContent, CardFooter, CardHeader, CardTitle} from '@/components
 import {useEffect, useState} from 'react';
 
 export default function TrendsList() {
-	// const [datatrends, setDatatrends] = useState([]);
+	const [datatrends, setDatatrends] = useState([]);
 
-	const datatrends = [
-		{title: '#hackatweet', nbOfTweets: 2},
-		{title: '#lacapsule', nbOfTweets: 1},
-		{title: '#fullstack', nbOfTweets: 5},
-	];
+	// const datatrends = [
+	// 	{title: '#hackatweet', nbOfTweets: 2},
+	// 	{title: '#lacapsule', nbOfTweets: 1},
+	// 	{title: '#fullstack', nbOfTweets: 5},
+	// ];
 
 	// TODO route
 	useEffect(() => {
 		fetch('http://localhost:3000/tweets')
 			.then((response) => response.json())
 			.then((data) => {
-				console.log(data.uniqueHashtagsArray);
-				for (let i = 0; i < data.uniqueHashtagsArray.length; i++) {
-					const num = 1;
-					const element = data.uniqueHashtagsArray[i];
-					const obj = {title: element, nbOfTweets: num};
-
-					// setDatatrends(obj);
-				}
-				// for (let i = 0; i < data.uniqueHashtagsArray; i++) {
-				// 	console.log(tr);
-				// }
+				console.log(data);
+				setDatatrends(data.uniqueHashtagsArray);
 			});
 	}, []);
 
@@ -34,8 +25,8 @@ export default function TrendsList() {
 	const displaydatatrends = datatrends.map((d, i) => {
 		return (
 			<div key={i} className="flex flex-col p-2">
-				<span>{d.title}</span>
-				<span>{d.nbOfTweets} Tweets</span>
+				<span className="italic text-blue-500">{d}</span>
+				{/* <span>{d} Tweets</span> */}
 			</div>
 		);
 	});
